e5f1b588f192d40fac9e8548e7918da4de708995
alenavino <alenavino1996@gmail.com>
2024-08-29 15:43:04 +0300

    fix parser

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -18,9 +18,8 @@ def parser_url(url):
     soup = BeautifulSoup(r.text, 'html.parser')
     h1 = soup.h1.string if soup.h1 else ''
     title = soup.title.string if soup.title else ''
+    description = ''
     for meta in soup.find_all('meta'):
         if meta.get('name') == 'description':
             description = meta.get('content')
-        else:
-            description = ''
     return {'h1': h1, 'title': title, 'description': description}
