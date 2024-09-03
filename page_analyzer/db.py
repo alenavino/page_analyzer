3762c0ea6461b332234c665cfcb8d5896535aa4a
alenavino <alenavino1996@gmail.com>
2024-09-03 11:09:49 +0300

    fix db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -12,118 +12,105 @@ def connect_db():
     return psycopg2.connect(DATABASE_URL)
 
 
-def get_urls_db():
+def get_dict(sql, param=None):
+    if param is None:
+        conn = connect_db()
+        with conn.cursor(cursor_factory=RealDictCursor) as curs:
+            curs.execute(sql)
+            all = curs.fetchall()
+        conn.commit()
+        conn.close()
+        return all
+    else:
+        conn = connect_db()
+        with conn.cursor(cursor_factory=RealDictCursor) as curs:
+            curs.execute(sql, param)
+            all = curs.fetchall()
+        conn.commit()
+        conn.close()
+        return all
+
+
+def get_element(sql, param):
     conn = connect_db()
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        curs.execute("SELECT urls.name, urls.id, last_url_checks.created_at,\
-                    last_url_checks.status_code FROM urls\
-                    LEFT JOIN last_url_checks\
-                    ON urls.id = last_url_checks.url_id\
-                    ORDER BY urls.id DESC;")
-        all_urls = curs.fetchall()
+    with conn.cursor() as curs:
+        curs.execute(sql, param)
+        res = curs.fetchone()
+    elem = res[0] if res else None
+    conn.commit()
     conn.close()
-    return all_urls
+    return elem
+
+
+def get_urls_db():
+    sql = "SELECT urls.name, urls.id, last_url_checks.created_at,\
+        last_url_checks.status_code FROM urls\
+        LEFT JOIN last_url_checks\
+        ON urls.id = last_url_checks.url_id\
+        ORDER BY urls.id DESC;"
+    return get_dict(sql)
 
 
 def get_url_db(id):
-    conn = connect_db()
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        curs.execute(
-            "SELECT * FROM urls WHERE id=%s;", (id,))
-        res = curs.fetchall()
-    url = res[0] if res else None
-    conn.close()
+    sql = "SELECT * FROM urls WHERE id=%s;"
+    param = (id, )
+    all = get_dict(sql, param)
+    url = all[0] if all else None
     return url
 
 
 def get_url_checks_db(id):
-    conn = connect_db()
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        curs.execute("SELECT * FROM url_checks WHERE url_id=%s \
-                    ORDER BY id DESC;", (id,))
-        checks = curs.fetchall()
-    conn.close()
-    return checks
+    sql = "SELECT * FROM url_checks WHERE url_id=%s \
+        ORDER BY id DESC;"
+    param = (id, )
+    return get_dict(sql, param)
 
 
 def get_id_url_db(url):
-    conn = connect_db()
-    with conn.cursor() as curs:
-        curs.execute(
-            "SELECT id FROM urls WHERE name=%s;", (url,))
-        res = curs.fetchone()
-    id_url = res[0] if res else None
-    conn.close()
-    return id_url
+    sql = "SELECT id FROM urls WHERE name=%s;"
+    param = (url, )
+    return get_element(sql, param)
 
 
 def add_new_url_db(url):
-    conn = connect_db()
-    with conn.cursor() as curs:
-        curs.execute(
-            "INSERT INTO urls (name, created_at) VALUES (%s, now()) \
-                RETURNING id;", (url, ))
-        id_url = curs.fetchone()[0]
-    conn.commit()
-    conn.close()
-    return id_url
+    sql = "INSERT INTO urls (name, created_at) VALUES (%s, now()) \
+        RETURNING id;"
+    param = (url, )
+    return get_element(sql, param)
 
 
 def get_name_url_db(id):
-    conn = connect_db()
-    with conn.cursor() as curs:
-        curs.execute(
-            "SELECT name FROM urls WHERE id=%s;", (id, ))
-        url = curs.fetchone()[0]
-    conn.close()
-    return url
+    sql = "SELECT name FROM urls WHERE id=%s;"
+    param = (id, )
+    return get_element(sql, param)
 
 
 def add_new_check_db(id, status, parsed_url):
-    conn = connect_db()
-    with conn.cursor() as curs:
-        curs.execute(
-            "INSERT INTO url_checks\
-            (url_id, created_at, status_code, h1, title, description)\
-            VALUES (%s, now(), %s, %s, %s, %s) RETURNING created_at;",
-            (id, status, parsed_url['h1'], parsed_url['title'], parsed_url['description'], ))
-        created_at = curs.fetchone()[0]
-    conn.commit()
-    conn.close()
-    return created_at
+    sql = "INSERT INTO url_checks\
+        (url_id, created_at, status_code, h1, title, description)\
+        VALUES (%s, now(), %s, %s, %s, %s) RETURNING created_at;"
+    param = (id, status, parsed_url['h1'], parsed_url['title'], parsed_url['description'])
+    return get_element(sql, param)
 
 
 def get_last_check_db(id):
-    conn = connect_db()
-    with conn.cursor() as curs:
-        curs.execute(
-            "SELECT id FROM last_url_checks WHERE url_id=%s;", (id, ))
-        res = curs.fetchone()
-    check = res[0] if res else None
-    conn.close()
-    return check
+    sql = "SELECT id FROM last_url_checks WHERE url_id=%s;"
+    param = (id, )
+    return get_element(sql, param)
 
 
 def update_last_check_db(id, created_at, status):
-    conn = connect_db()
-    with conn.cursor() as curs:
-        curs.execute(
-            "UPDATE last_url_checks\
-            SET created_at=%s, status_code=%s\
-            WHERE url_id=%s;", (created_at, status, id, ))
-    conn.commit()
-    conn.close()
+    sql = "UPDATE last_url_checks SET created_at=%s, status_code=%s\
+        WHERE url_id=%s RETURNING id;"
+    param = (created_at, status, id)
+    return get_element(sql, param)
 
 
 def add_last_check_db(id, created_at, status):
-    conn = connect_db()
-    with conn.cursor() as curs:
-        curs.execute(
-            "INSERT INTO last_url_checks\
-            (url_id, created_at, status_code)\
-            VALUES (%s, %s, %s);", (id, created_at, status))
-    conn.commit()
-    conn.close()
+    sql = "INSERT INTO last_url_checks (url_id, created_at, status_code)\
+        VALUES (%s, %s, %s) RETURNING id;"
+    param = (id, created_at, status)
+    return get_element(sql, param)
 
 
 def last_check_db(id, created_at, status):
